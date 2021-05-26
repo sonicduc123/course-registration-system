58a16c6162592ed4f68527032f4b95f8fc0c3484
sonicduc123 <sonicduc123@gmail.com>
2021-05-27 00:19:57 +0700

    add HibernateUtil

--- src/util/HibernateUtil.java
+++ src/util/HibernateUtil.java
@@ -1,4 +1,21 @@
 package util;
 
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class HibernateUtil {
+    private static final SessionFactory sessionFactory;
+    static {
+        try {
+
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+        } catch (HibernateException e) {
+            System.err.println("Initial SessionFactory creation failed." + e);
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
 }
